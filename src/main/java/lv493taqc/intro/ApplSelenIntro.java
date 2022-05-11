4e31aed5263ec88843a1f2d2c40a2816f107b650
oleh bohdan <bomemailn@gmail.com>
2022-05-11 08:50:33 +0300

    lv493 2-nd

--- src/main/java/lv493taqc/intro/ApplSelenIntro.java
+++ src/main/java/lv493taqc/intro/ApplSelenIntro.java
@@ -1,10 +1,23 @@
 package lv493taqc.intro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ApplSelenIntro {
 
 	public static void main(String[] args) {
 		
+		//System.out.println("selenium start");
+		
+        System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://softserve.academy/");
+		driver.findElement(By.linkText("Log in")).click();
 		System.out.print("selenium done");
-
 	}
 
 }
