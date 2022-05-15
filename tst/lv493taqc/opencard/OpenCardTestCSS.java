ffa0b4a36964425c2df56de4d1d8ffaf60e2f80c
oleh bohdan <bomemailn@gmail.com>
2022-05-15 20:52:31 +0300

    change

--- tst/lv493taqc/opencard/OpenCardTestCSS.java
+++ tst/lv493taqc/opencard/OpenCardTestCSS.java
@@ -13,7 +13,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class OpenCardTestUpdated {
+public class OpenCardTestCSS {
 
 	private WebDriver driver;
 	
