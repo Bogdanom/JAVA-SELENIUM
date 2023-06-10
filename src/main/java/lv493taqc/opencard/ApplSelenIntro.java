2677dfbbe9772a72d9ea4bdfdcd69b441abc32d5
oleh bohdan <bomemailn@gmail.com>
2023-06-10 12:36:31 +0300

    refresh chrome driver version

--- src/main/java/lv493taqc/opencard/ApplSelenIntro.java
+++ src/main/java/lv493taqc/opencard/ApplSelenIntro.java
@@ -19,22 +19,30 @@ public class ApplSelenIntro {
 		driver.manage().window().maximize();
 	
 		driver.get("https://www.opencart.com/");
-		System.out.println("selenium start done, page title is - "+driver.getTitle());
+		Thread.sleep(2000);
 		
+		System.out.println("selenium start done, page title is - "+driver.getTitle());
+		Thread.sleep(2000);
+		
+		// click login button
 		driver.findElement(By.xpath("//header/nav[1]/div[1]/div[2]/div[1]/a[1]")).click();
+		Thread.sleep(2000);
 		
 		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // deprecated 
 		
-		//login
+		//login - put email into user field
 		//driver.findElement(By.xpath("//input[@id='input-email']")).click();
 		driver.findElement(By.id("input-email")).click();
 		driver.findElement(By.xpath("//input[@id='input-email']")).clear();
 		driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("bomemailn@gmail.com");
+		Thread.sleep(2000);
 		
+		//login - put password into password field
 		//driver.findElement(By.xpath("//input[@id='input-password']")).click();
 		driver.findElement(By.id("input-password")).click();
 		driver.findElement(By.xpath("//input[@id='input-password']")).clear();
 		driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("Asdfg12@", Keys.ENTER);
+		Thread.sleep(2000);
 		
 		//next line(click on the login button) is substituted by Keys.ENTER
 		//driver.findElement(By.xpath("//body/div[@id='account-login']/div[2]/div[1]/div[1]/form[1]/div[3]/div[1]/button[1]")).click();
@@ -43,24 +51,26 @@ public class ApplSelenIntro {
 		WebElement  alert1 = driver.findElement(By.xpath("//body/div[@id='account-login']/div[2]/div[1]"));
 		System.out.println("Message - " + alert1.getText());
 		*/
-		
-		/* 	*/
+		// put pin-code into pin-code field
 		driver.findElement(By.xpath("//input[@id='input-pin']")).click();
 		driver.findElement(By.xpath("//input[@id='input-pin']")).clear();
 		driver.findElement(By.xpath("//input[@id='input-pin']")).sendKeys("7654", Keys.ENTER);
+		Thread.sleep(2000);
+		
 		
 		//next line(click on the login button) is substituted by Keys.ENTER
 		//driver.findElement(By.xpath("//*[@id=\"account-security\"]/div[2]/div/div[1]/form/div[2]/button")).click();
 		
 		//
-		/* works(print the message) if pin is/are wrong
+		/* works(print the message) if the pin is wrong
 		WebElement  alert2 = driver.findElement(By.xpath("//*[@id=\"account-security\"]/div[2]/div/div[1]/div"));
 		System.out.println("Message - " + alert2.getText());
 		*/
 
+		// click logout button
 		driver.findElement(By.xpath("//header/nav[1]/div[1]/div[2]/div[1]/a[2]")).click();//logout
+		Thread.sleep(2000); //pause to see result, don't use in work
 		
-		//Thread.sleep(2000); //pause to see result, don't use in work
 		//driver.close(); //close browser only, but not stop web driver in memory
 		driver.quit();// close browser and stop web driver in memory
 		System.out.print("appl done");
