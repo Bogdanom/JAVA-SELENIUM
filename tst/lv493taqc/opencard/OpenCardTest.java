b86f090083fed7880eb218daca19b815a49d2d0e
oleh bohdan <bomemailn@gmail.com>
2023-06-10 17:06:23 +0300

    check gitignore

--- tst/lv493taqc/opencard/OpenCardTest.java
+++ tst/lv493taqc/opencard/OpenCardTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 public class OpenCardTest {
 	
-	@Test
+	//@Test
 	public void unsuccessfulLoginEmail() {
 //this test check if we have appropriate error message in case of log in with wrong email
 		
@@ -41,7 +41,7 @@ public class OpenCardTest {
 		
 		driver.quit();
 	}
-	//@Test
+	@Test
 	public void unsuccessfulLoginPassword() {
 //this test check if we have appropriate error message in case of log in with wrong Password
 		
