3d0bfb4c9dd143fd0ba2b1548e30985d5ca4a4be
oleh bohdan <bomemailn@gmail.com>
2023-10-08 18:16:27 +0300

    c

--- tst/opencart/tests/SmokeTest.java
+++ tst/opencart/tests/SmokeTest.java
@@ -14,7 +14,7 @@ import opencart.pages.guest.SuccessLoginPage;
 
 public class SmokeTest extends OpenCartTestRunner {
 
-	//@Test
+	// @Test
 	public void smokeTest() {
 
 		HomePage homePage = loadApplication();
@@ -27,86 +27,94 @@ public class SmokeTest extends OpenCartTestRunner {
 		System.out.println("== Logo = " + homePage.getLogoText());
 		System.out.println("== Register = " + homePage.getRegisterText());
 		System.out.println("== Login = " + homePage.getLoginText());
-		
+
 		homePage.clickLogin();
 		delay();
-		
+
 		System.out.println("=====================");
 	}
 
 	@DataProvider
-	public Object[][] users() {
-		return new Object[][] {
-			{ UserRepository.get().wrongUser() }
-		};
+	public Object[][] wrongUsers() {
+		return new Object[][] { 
+			{ UserRepository.get().wrongCreds() }, 
+			{ UserRepository.get().wrongEmail() },
+			{UserRepository.get().wrongPassword() }, 
+			{UserRepository.get().emptyCreds() }
+	   };
 	}
-	
-	@Test(dataProvider = "users")
-	public void loginPage(IUser user) {
+
+	@Test(dataProvider = "wrongUsers")
+	public void loginPage(IUser wrongUser) {
 		// check appropriate messages - log in with wrong credentials
 
 		FailedLoginPage failedLogin = loadApplication()
-		.clickLogin()
-		.setEmail(user)
-		.setPassword(user)
-		.clickSubmitNotlogin();
-		
+				.clickLogin()
+				.setEmail(wrongUser)
+				.setPassword(wrongUser)
+				.clickSubmitNotlogin();
+
 		System.out.println("== LoginPage ==");
-		
+
 		String expected = failedLogin.WARNING_MESSAGE;
 		int strLength = expected.length();
-		
+
 		String actual = failedLogin.getAlertText();
 		actual = actual.substring(0, strLength);
-		
+
 		System.out.println("== Warning Message actual - " + actual);
 		Assert.assertEquals(expected, actual);
-		
+
 		System.out.println("== Question Message - " + failedLogin.getQuestionText());
 		Assert.assertTrue(failedLogin.getQuestionText().contains(failedLogin.QUESTION_MESSAGE));
-				
+
 		failedLogin.clickLogo();
 		delay();
-		
+
 		System.out.println("=====================");
 	}
-	
-	//@Test
+
+	// @Test
 	public void registerPage() {
-		
-        //check Register Page
+
+		// check Register Page
 
 		RegisterPage registerPage = loadApplication().clickRegister();
 		delay(2);
-		
+
 		System.out.println("== RegisterPage ==");
 		System.out.println("== Email Text - " + registerPage.getEmailText());
 		System.out.println("== Password Text - " + registerPage.getPasswordText());
-		
+
 		Assert.assertTrue(registerPage.isDisplayedEmail());
 		Assert.assertTrue(registerPage.isDisplayedPassword());
-		
+
 		System.out.println("=====================");
-	
+
 	}
 	
-	//@Test
+	@DataProvider
+	public Object[][] correctUsers() {
+		return new Object[][] { { 
+			UserRepository.get().correctUser() 
+	  } };
+	}
+	//@Test(dataProvider = "correctUsers")
 	// work if fast pin inputting 7654 (manual)
 	// task - catch page for security pin inputing
-	public void SuccessfulLogin() {
+	public void SuccessfulLogin(IUser correctUser) {
 		// check login feature - log in with right credentials
 
-		SuccessLoginPage okLogin = loadApplication()
-		.clickLogin()
-	//	.setEmail("bomemailn@gmail.com")
-	//	.setPassword("Asdfg12@")
-		.clickSubmitLogin();
+		SuccessLoginPage okLogin = loadApplication().clickLogin()
+				.setEmail(correctUser)
+				.setPassword(correctUser)
+				.clickSubmitLogin();
 
 		delay();
-		
+
 		System.out.println("== Account Text - " + okLogin.getAccountText());
-		
-		Assert.assertTrue(okLogin.getAccountText().contains(okLogin.TITLE));		
+
+		Assert.assertTrue(okLogin.getAccountText().contains(okLogin.TITLE));
 
 		delay(2);
 	}
